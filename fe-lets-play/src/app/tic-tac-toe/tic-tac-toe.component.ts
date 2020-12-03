a2792129983045559706d44772b107c6fbda9386
gabriela <gabskatzedev@gmail.com>
2020-12-03 12:23:04 -0500

    game 1 version

---
+++ fe-lets-play/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tic-tac-toe',
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrls: ['./tic-tac-toe.component.css']
+})
+export class TicTacToeComponent implements OnInit {
+
+  public values: string[] = ["", "", "", "", "", "", "", "", ""];
+  private currentValue: string = "X";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  switchValue(index: number){
+    
+    if(this.values[index] !== ""){
+      return;
+    }
+
+    this.values[index] = this.currentValue;
+    
+    if(this.currentValue === "X"){
+      this.currentValue = "O";
+    } else {
+      this.currentValue = "X";
+    }
+  }
+
+  ngOnChanges(){
+    console.log("ngOnChanges");
+    //this.switchValue();
+  }
+}
